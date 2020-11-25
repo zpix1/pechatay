a758b4cf1aea9d68052379cfe5b7a6800d5655c1
Ivan Baksheev <zpix-dev@list.ru>
2020-11-25 17:51:43 +0700

    prototype

---
+++ src/components/Main.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container">
+    <Typer/>
+  </div>
+</template>
+
+<script>
+import Typer from './Typer';
+
+export default {
+  name: 'Main',
+  components: {
+    Typer
+  }
+}
+</script>
+
+<style scoped>
+
+.container {
+  max-width: 600px;
+  margin: 0 auto;
+}
+
+</style>
---
+++ src/components/Typer.vue
@@ -0,0 +1,27 @@
+<template>
+  <h1>Typer</h1>
+  <div>
+    <div></div>
+    <div contenteditable="true" class="typingplace"></div>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Typer',
+  data: () => {
+    return {
+      'text': 'Привет, это тестирование набора текста.'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+.typingplace {
+  border: 1px solid black;
+}
+
+</style>
